eefc7e10cac57faf16a9958cbba542dffa1a8858
Enrique Hernández Sánchez <77438261+EnriqueHernandezSanchez@users.noreply.github.com>
2021-06-15 22:25:00 +0200

    final main.py

--- main.py
+++ main.py
@@ -4,40 +4,81 @@ from helperfunctions import random_inputs_outputs_test_generator, RBF, prior, po
 
 # Dataset and cov matrix (for posterior)
 
-N = 10 # Number of data points
+N = 3   # Number of data points
+N_test = 40 # Number of test points
 gen_var = 1.0 # variance in the generation of data points
 
-X, Y, Xt, Yt = random_inputs_outputs_test_generator(N, gen_var, seed = 4)
+X, Y, Xt, Yt = random_inputs_outputs_test_generator(N, gen_var, N_test)
 
+plt.figure(0)
 plt.plot(X, Y, 'ro', mew=2)
-
 
 
 # Prior and samples
 
-samples = 10 # Number of samples to plot
+n_samples = 5 # Number of samples to plot
+var = 1.0
+lengthscale = 1.0
 
-#Xplot = np.expand_dims(np.linspace(-7, 7, 80), 1) # 80 points in each sample by default
-#Kprior = RBF(Xplot, Xplot)
-#mean = np.zeros(len(Xplot))
+plt.figure(1)
+plt.title("Prior samples")
+plt.plot(X, Y, 'ro', mew=2)
 
-prior(RBF, samples)
+Xplot = np.expand_dims(np.linspace(0, 5, 100), 1) # 100 points in each sample by default
 
+prior = prior(RBF, Xplot, var, lengthscale, n_samples)
+
+plt.ylim([-3, 3])
+    
+
+# plot prior samples
+    
+for i in range(n_samples):
+    plt.plot(Xplot, prior[i], linestyle = '-', marker = 'x', markersize = 0)
+ 
+plt.plot(Xplot, np.zeros(len(Xplot)), linestyle = '-', color ='blue')       
+plt.plot(Xplot, var * np.ones(len(Xplot)), linestyle = 'dashed', color = 'red')
+plt.plot(Xplot, -1.0 * var * np.ones(len(Xplot)), linestyle = 'dashed', color = 'red')
+plt.legend(['training points', "mean", 'standard dev.'], loc ="upper right")
 
 
 # Posterior
 
 # Covariance Matrices
 
-#K_XX = Kprior # , var = 1.0, lengthscale = 1.0)
-K_XtXt = RBF(Xt, Xt) # , var = 1.0, lengthscale = 1.0)
-K_XXt = RBF(X, Xt) # , var = 1.0, lengthscale = 1.0)
+K_XX = RBF(X, X)
+K_XtXt = RBF(Xt, Xt)
+K_XXt = RBF(X, Xt)
 
-posterior(RBF, Y) #, var = 1.0, lengthscale = 1.0, n_samples = 5)
+mean_posterior, cov_posterior = posterior(RBF, X, Y, Xt, var = 1.0, lengthscale = 1.0)
 
-# mean_posterior, cov_posterior = posterior (K_XX, K_XXt, K_XtXt, Y, var = 1.0, lengthscale = 1.0)
-# print(mean_posterior.T[0])
-# print(mean)
+mean_posterior_plot, cov_posterior_plot = posterior(RBF, X, Y, Xplot)
+
+posterior_plot = np.random.multivariate_normal(mean_posterior_plot.T[0], cov_posterior_plot, n_samples)
+
+yplot_var = cov_posterior_plot.diagonal
+print(yplot_var)
+
+plt.figure(2)
+plt.title("Mean posterior")
+plt.plot(X, Y, 'ro', mew=2)
+plt.plot(Xplot, mean_posterior_plot.T[0], linestyle = '-', marker = 'x', markersize = 0, color = 'blue')
+
+plt.figure(3)
+plt.title("Posterior samples")
+plt.plot(X, Y, 'ro', mew=2)
+for i in range(n_samples):    
+    plt.plot(Xplot, posterior_plot[i], linestyle = '-', marker = 'x', markersize = 0)
     
+covariances = cov_posterior_plot.diagonal()
 
+f = np.sin(2*Xplot) + np.cos(Xplot)
 
+plt.figure(4)
+plt.title("Posterior distribution")
+plt.plot(X, Y, 'ro', mew=2,markersize = 7)# label='_nolegend_')                
+plt.plot(Xplot, f, linestyle = '-' ,color = 'green', markersize = 30)
+plt.plot(Xplot, mean_posterior_plot.T[0], linestyle = '-', marker = 'x', markersize = 0 ,color ='blue')
+plt.plot(Xplot, mean_posterior_plot.T[0] + covariances, linestyle = 'dashed', color = 'red')
+plt.plot(Xplot, mean_posterior_plot.T[0] - covariances, linestyle = 'dashed', color = 'red')
+plt.legend(['training points', 'f(x)', "mean function", 'standard dev.'], loc ="upper right")
